7fd2c44eb4690b18b5059cadf000d2113403eaed
sneha <sneharaj809@gmail.com>
2026-03-18 19:04:12 +0530

    new update in the base

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/crossword-game/', // This must match your repository name
-})
-
-
-// https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+//   base: '/crossword-game/', // This must match your repository name
 // })
+
+
+//https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
