7ad472ed9b3128e9b258c4d0fd2917b022939588
sneha <sneharaj809@gmail.com>
2026-03-18 18:50:43 +0530

    Fix: added base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: '/crossword-game/', // This must match your repository name
 })
+
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
